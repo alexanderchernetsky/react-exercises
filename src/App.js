c2daedd23061650f7f1722b921a2f54c62cb382d
Alexander Chernetsky <alexvnderchernetsky@gmail.com>
2019-03-23 22:39:18 +0300

    Correct App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, {Component, Suspense, lazy} from 'react';
 import {BrowserRouter, Route} from "react-router-dom";
 import './App.css';
-import Header from './components/Header';
-import Home from './components/Home';
+const Header = lazy(() => import('./components/Header'));
+const Home = lazy(() => import ('./components/Home'));
 const Exercise1 = lazy(() => import ('./components/Exercise1'));
 const Exercise2 = lazy(() => import ('./components/Exercise2'));
 const Exercise3 = lazy(() => import ('./components/Exercise3'));
@@ -11,16 +11,16 @@ const Exercise4 = lazy(() => import ('./components/Exercise4'));
 class App extends Component {
   render() {
     return (
-        <BrowserRouter>
-          <Suspense fallback={<div>loading...</div>}>
-            <Header/>
-            <Route exact path='/' component={Home}/>
-            <Route path='/exercise1' component={Exercise1}/>
-            <Route path='/exercise2' component={Exercise2}/>
-            <Route path='/exercise3' component={Exercise3}/>
-            <Route path='/exercise4' component={Exercise4}/>
-          </Suspense>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Suspense fallback={<div>loading...</div>}>
+          <Header/>
+          <Route exact path='/' component={Home}/>
+          <Route path='/exercise1' component={Exercise1}/>
+          <Route path='/exercise2' component={Exercise2}/>
+          <Route path='/exercise3' component={Exercise3}/>
+          <Route path='/exercise4' component={Exercise4}/>
+        </Suspense>
+      </BrowserRouter>
     );
   }
 }
